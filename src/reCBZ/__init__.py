e0337ddac3b7d19b715893be2d4e5793e8c03562
gwyn <29720696+avalonv@users.noreply.github.com>
2022-11-10 08:17:46 -0300

    bump version 0.1.0 -> 0.2.0
    reset version. sorry
    
    we haven't a completely defined API yet

--- src/reCBZ/__init__.py
+++ src/reCBZ/__init__.py
@@ -4,7 +4,7 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
-__version__ = "1.0.0"
+__version__ = "0.2.0"
 CMDNAME = 'recbz'
 
 _cfg = tomllib.loads(resources.read_text("reCBZ", "config.toml"))
