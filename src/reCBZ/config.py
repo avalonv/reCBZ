0b0d747727ad0a45766b6a4e0b6d423f796787fc
gwyn <29720696+avalonv@users.noreply.github.com>
2022-11-12 17:10:03 -0300

    config: check OSError in term_width

--- src/reCBZ/config.py
+++ src/reCBZ/config.py
@@ -83,13 +83,13 @@ class Config():
     @property
     def term_width(cls) -> int:
         # limit output message width. ignored if verbose
-        TERM_COLUMNS, TERM_LINES = os.get_terminal_size()
         try:
+            TERM_COLUMNS, TERM_LINES = os.get_terminal_size()
             assert TERM_COLUMNS > 0 and TERM_LINES > 0
             if TERM_COLUMNS > 120: max_width = 120
             elif TERM_COLUMNS < 30: max_width = 30
             else: max_width = TERM_COLUMNS - 2
-        except AssertionError:
+        except (AssertionError, OSError):
             print("[!] Can't determine terminal size, defaulting to 78 cols")
             max_width = 78
         return max_width
