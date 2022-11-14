446d128b756d2b0a2328e4c2b7dfae8c1a61f30c
gwyn <29720696+avalonv@users.noreply.github.com>
2022-11-14 09:13:24 -0300

    bump version 0.4.0 -> 0.4.1

--- src/reCBZ/__init__.py
+++ src/reCBZ/__init__.py
@@ -4,7 +4,7 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 CMDNAME = 'recbz'
 
 _cfg = tomllib.loads(resources.read_text("reCBZ", "config.toml"))
