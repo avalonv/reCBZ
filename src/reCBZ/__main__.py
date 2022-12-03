eeb40964f8125d78cafa23e74937fb40370a74c6
gwyn <29720696+avalonv@users.noreply.github.com>
2022-12-02 23:12:50 -0300

    fix: set grayscale based on profile
    default argparse vars to None. they appear to default to false if store_true and vice versa, which is very bad

--- src/reCBZ/__main__.py
+++ src/reCBZ/__main__.py
@@ -52,52 +52,63 @@ def main():
     process_group = parser.add_mutually_exclusive_group()
     # TODO clean leftover options
     parser.add_argument( "-nw", "--nowrite",
+        default=None,
         dest="nowrite",
         action="store_true",
         help="dry run, no changes are saved at the end (safe)")
     ext_group.add_argument( "-O", "--overwrite",
+        default=None,
         dest="overwrite",
         action="store_true",
         help="overwrite the original archive")
     parser.add_argument( "-F", "--force",
+        default=None,
         dest="ignore",
         action="store_true",
         help="ignore file errors when writing pages (dangerous)")
     log_group.add_argument( "-v", "--verbose",
+        default=None,
         dest="loglevel",
         action="count",
         help="increase verbosity of progress messages, repeatable: -vvv")
     log_group.add_argument( "-s", "--silent",
+        default=None,
         const=-1,
         dest="loglevel",
         action="store_const",
         help="disable all progress messages")
     # mode_group.add_argument( "-u", "--unpack",
+    #     default=None,
     #     const='unpack',
     #     dest="mode",
     #     action="store_const",
     #     help="unpack the archive to the currect directory (safe)")
     mode_group.add_argument( "-c", "--compare",
+        default=None,
         const='compare',
         dest="mode",
         action="store_const",
         help="calculate filesize when converting to each image format (safe)")
     mode_group.add_argument( "-a", "--assist",
+        default=None,
         const='assist',
         dest="mode",
         action="store_const",
         help="compare, then ask which format to repack with")
     mode_group.add_argument( "-A" ,"--auto",
+        default=None,
         const='auto',
         dest="mode",
         action="store_const",
         help="compare, then automatically pick the best format to repack with")
     mode_group.add_argument( "-J" ,"--join",
+        default=None,
         const='join',
         dest="mode",
         action="store_const",
         help="append the contents of each file to the first/leftmost file")
     parser.add_argument( "-p", "--profile",
+        default=None,
         metavar="",
         dest="profile",
         type=str,
@@ -108,21 +119,25 @@ def main():
         action='store_true',
         help=argparse.SUPPRESS)
     imgfmt_group.add_argument( "--nowebp",
+        default=None,
         const=f'{Config.blacklistedfmts} webp webpll',
         dest="blacklistedfmts",
         action="store_const",
         help="exclude webp from --auto and --assist")
     ext_group.add_argument( "--epub",
+        default=None,
         const='epub',
         dest="bookformat",
         action="store_const",
         help="save archive as epub")
     ext_group.add_argument( "--zip",
+        default=None,
         const='zip',
         dest="outformat",
         action="store_const",
         help="save archive as zip")
     ext_group.add_argument( "--cbz",
+        default=None,
         const='cbz',
         dest="outformat",
         action="store_const",
@@ -133,54 +148,64 @@ def main():
         action="store_true",
         help="ignore previously repacked files")
     parser.add_argument( "--compress",
+        default=None,
         dest="compresszip",
         action="store_true",
         help="attempt to further compress the archive when repacking")
     process_group.add_argument("--process",
+        default=None,
         choices=(range(1,33)),
         metavar="1-32",
         dest="processes",
         type=int,
         help="maximum number of processes to spawn")
     process_group.add_argument( "--sequential",
+        default=None,
         dest="parallel",
         action="store_false",
         help="disable multiprocessing")
     imgfmt_group.add_argument( "--imgfmt",
+        default=None,
         choices=('jpeg', 'png', 'webp', 'webpll'),
         metavar="format",
         dest="imageformat",
         type=str,
         help="format to convert pages to: jpeg, webp, webpll, or png")
     parser.add_argument( "--quality",
+        default=None,
         choices=(range(1,101)),
         metavar="0-95",
         dest="quality",
         type=int,
         help="save quality for lossy formats. >90 not recommended")
     parser.add_argument( "--size",
+        default=None,
         metavar="WidthxHeight",
         dest="size_str",
         type=str,
         help="rescale images to the specified resolution")
     parser.add_argument( "--noup",
+        default=None,
         dest="noupscale",
         action="store_true",
         help="disable upscaling with --size")
     parser.add_argument( "--nodown",
+        default=None,
         dest="nodownscale",
         action="store_true",
         help="disable downscaling with --size")
     parser.add_argument( "--bw",
-        default=Config.grayscale,
+        default=None,
         dest="grayscale",
         action="store_true",
         help="convert images to grayscale")
     parser.add_argument( "--config",
+        default=None,
         dest="show_config",
         action="store_true",
         help="show current settings and exit")
     parser.add_argument( "--version",
+        default=None,
         dest="show_version",
         action="store_true",
         help="show version and exit")
